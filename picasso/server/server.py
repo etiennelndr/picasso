fe820268035fad8d105c8a69ef8ceff4a45991eb
Raphaël Landuré <raph.landure@gmail.com>
2020-04-15 15:10:40 +0200

    Rephrasing some titles (in html)

--- picasso/server/server.py
+++ picasso/server/server.py
@@ -9,6 +9,7 @@ def english_version():
     print("switching_to_english_version")
     return flask.render_template("camera.en.html")
 
+
 @app.route('/camera.fr')
 def french_version():
     print("switching_to_french_version")
